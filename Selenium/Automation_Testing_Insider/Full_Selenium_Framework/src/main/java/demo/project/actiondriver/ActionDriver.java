2050a7eb283e2df3de6449b4317ea83eec3cbb47
ignant1 <105660204+ignant1@users.noreply.github.com>
2026-06-30 11:35:45 -0700

    2026-06-30 Part 10
    Added better Element description information to logging

--- Selenium/Automation_Testing_Insider/Full_Selenium_Framework/src/main/java/demo/project/actiondriver/ActionDriver.java
+++ Selenium/Automation_Testing_Insider/Full_Selenium_Framework/src/main/java/demo/project/actiondriver/ActionDriver.java
@@ -14,123 +14,130 @@ import demo.project.base.BaseClass;
 
 public class ActionDriver{
 	
-	private WebDriver driver;
+	private WebDriver webDr;
 	private WebDriverWait wait;
 	public static final Logger logger = BaseClass.logger;
 	
 	//Constructor for when the class gets created
 	public ActionDriver(WebDriver wb) {
 		logger.info("Action Driver constructor called.");
-		driver = wb;
-		wait = new WebDriverWait(driver, Duration.ofSeconds(Integer.parseInt(BaseClass.getProperties().getProperty("actiondriverwait"))));
+		webDr = wb;
+		wait = new WebDriverWait(webDr, Duration.ofSeconds(Integer.parseInt(BaseClass.getProperties().getProperty("actiondriverwait"))));
 		logger.info("Setup the WebDriver wait.");
 	}
 	
 	//method to click an element
 	public void click(By by) {
-		logger.info("Running the Action Driver click method." + by.toString());
+		String description = getElementDescription(by);
+		logger.info("Running the Action Driver click method for " + by.toString());
 		try {
 			waitForElementToBeClickable(by);
-			driver.findElement(by).click();
-			logger.info("performed the click");
+			webDr.findElement(by).click();
+			logger.info("Clicked Element: " + description);
 		} catch (Exception e) {
-			logger.info("Unable to click the element: " + e.getMessage());
-			logger.error("Unable to click the element: " + e.getMessage());
+			logger.error("Unable to click the element: " + description);
+			logger.error("Error message: " + e.getMessage());
 		}
 	}
 	
 	//method to enter text into textbox
 	public void enterText(By by, String text) {
-		logger.info("Running the Action Driver enterText method" + by.toString());
+		String description = getElementDescription(by);
+		logger.info("Running the Action Driver enterText method for " + by.toString());
 		logger.info("The text to enter is \'" + text + "\'");
 		try {
 			waitForElementToBeVisible(by);
 			
 			//Assigning element to variable to handle functionality
 			//no longer going to do it directly in the driver
-			WebElement element = driver.findElement(by);
+			WebElement element = webDr.findElement(by);
 			//driver.findElement(by).clear();
 			//driver.findElement(by).sendKeys(text);
 			element.clear();
 			element.sendKeys(text);
+			logger.info("Entering text into element: " + description);
 			logger.info("entered the text: \'" + text + "\'");
 		} catch (Exception e) {
-			logger.info("Unable to enter text into textbox: " + e.getMessage());
-			logger.error("Unable to enter text into textbox: " + e.getMessage());
+			logger.error("Unable to enter text into textbox: " + description);
+			logger.error("Error Message: " + e.getMessage());
 		}
 	}
 	
 	//method to get text from textbox
 	public String getText(By by) {
-		logger.info("Running the Action Driver getText method");
+		String description = getElementDescription(by);
+		logger.info("Running the Action Driver getText method for " + by.toString());
 		try {
 			waitForElementToBeVisible(by);
-			logger.info("returning the text from the element.");
-			return driver.findElement(by).getText();
+			logger.info("returning the text from the element: " + description);
+			return webDr.findElement(by).getText();
 		} catch (Exception e) {
-			logger.info("Unable to get text from textbox: " + e.getMessage());
-			logger.error("Unable to get text from textbox: " + e.getMessage());
+			logger.error("Unable to get text from textbox: " + description);
+			logger.error("Error Message: " + e.getMessage());
 			return "";
 		}
 	}
 	
 	//method to compare two texts
 	public boolean compareText(By by, String expectedText) {
+		String description = getElementDescription(by);
 		logger.info("Running the Action Driver compareText method" + by.toString());
 		logger.info("The text to compare is \'" + expectedText + "\'");
 		try {
 			waitForElementToBeVisible(by);
-			String actualText = driver.findElement(by).getText();
+			String actualText = webDr.findElement(by).getText();
 			if(expectedText.equals(actualText)) {
-				logger.info("Text Matches:");
+				logger.info("Text Matches in the element: " + description);
 				logger.info("Actual Text: \"" + actualText + "\" matches Expected Text : \""
 						+ expectedText + "\" ");
 				return true;
 			} else {
-				logger.info("Text Does Not Match:");
+				logger.info("Text Does Not Match in the element: " + description);
 				logger.info("Actual Text: \"" + actualText + "\" does not match Expected Text : \""
 						+ expectedText + "\" ");
 				return false;
 			}
 		} catch (Exception e) {
-			logger.info("Unable to get text from textbox: " + e.getMessage());
-			logger.error("Unable to get text from textbox: " + e.getMessage());
+			logger.error("Unable to get text from textbox: " + description);
+			logger.error("Error Message: " + e.getMessage());
 			return false;
 		}
 	}
 	
 	//method to check if element is displayed
 	public boolean isDisplayed(By by){
+		String description = getElementDescription(by);
 		logger.info("Running the Action Driver isDisplayed method" + by.toString());
 		try {
 			waitForElementToBeVisible(by);
 			
 			//going to simplify the code to just return boolean isDisplayed, not going to include a printout
-			logger.info("Returning boolean if the element is displayed or not.");
-			return driver.findElement(by).isDisplayed();
+			logger.info("Returning boolean if the element, " + description + " is displayed or not.");
+			return webDr.findElement(by).isDisplayed();
 			/*
 			 * boolean isDisplayed = driver.findElement(by).isDisplayed(); if(isDisplayed) {
 			 * System.out.println("Element is visible"); return isDisplayed; } else {
 			 * System.out.println("Element is not visible."); return isDisplayed; }
 			 */
 		} catch (Exception e) {
-			logger.info("Element is not visible: " + e.getMessage());
-			logger.error("Element is not visible: " + e.getMessage());
+			logger.error("Element is not visible: " + description);
+			logger.error("Error message: " + e.getMessage());
 			return false;
 		}
 	}
 	
 	//scroll to an element
 	public void scrollToElement(By by) {
+		String description = getElementDescription(by);
 		logger.info("Running the Action Driver scrollToElement method" + by.toString());
 		try {
 			waitForElementToBeVisible(by);
-			JavascriptExecutor jse = (JavascriptExecutor) driver;
-			jse.executeScript("arguments[0].scrollIntoView(true);", driver.findElement(by));
-			logger.info("Scrolled to the element.");
+			JavascriptExecutor jse = (JavascriptExecutor) webDr;
+			jse.executeScript("arguments[0].scrollIntoView(true);", webDr.findElement(by));
+			logger.info("Scrolled to the element: " + description);
 		} catch (Exception e) {
-			logger.info("Unable to scroll to the element: " + e.getMessage());
-			logger.error("Unable to scroll to the element: " + e.getMessage());
+			logger.error("Unable to scroll to the element: " + description);
+			logger.error("Error Message: " + e.getMessage());
 		}
 	}
 	
@@ -149,25 +156,82 @@ public class ActionDriver{
 	
 	//wait for element to be clickable
 	private void waitForElementToBeClickable(By by) {
+		String description = getElementDescription(by);
 		logger.info("Running the Action Driver waitforElementToBeClickable method: " + by.toString());
 		try {
 			wait.until(ExpectedConditions.elementToBeClickable(by));
-			logger.info("Element is now clickable.");
+			logger.info("Element is now clickable: " + description);
 		} catch (Exception e) {
-			logger.info("Element is not clickable: " + e.getMessage());
-			logger.error("Element is not clickable: " + e.getMessage());
+			logger.error("Element is not clickable: " + description);
+			logger.error("Error message: " + e.getMessage());
 		}
 	}
 	
 	//wait for element to be visible
 	private void waitForElementToBeVisible(By by) {
-		logger.info("Running the Action Driver waitForElementToBeVisible method" + by.toString());
+		String description = getElementDescription(by);
+		logger.info("Running the Action Driver waitForElementToBeVisible method: " + by.toString());
 		try {
 			wait.until(ExpectedConditions.visibilityOfElementLocated(by));
-			logger.info("Element is now visible.");
+			logger.info("Element is now visible: " + description);
 		} catch (Exception e) {
-			logger.info("Element is not visible: " + e.getMessage());
-			logger.error("Element is not visible: " + e.getMessage());
+			logger.error("Element is not visible: " + description);
+			logger.error("Error Message: " + e.getMessage());
+		}
+	}
+	
+	//method to get the description of the element by locator
+	public String getElementDescription(By locator) {
+		//check for null driver or locator to avoid null pointer exception
+		if(webDr==null) {
+			return "Web Driver was not intialized.";
+		}
+		if(locator==null) {
+			return "Locator was not provided.";
+		}
+		
+		//find the element
+		WebElement element = webDr.findElement(locator);
+		
+		//get the element attribute
+		String name = element.getDomAttribute("name");
+		String id = element.getDomAttribute("id");
+		String text = element.getText();
+		String className = element.getDomAttribute("class");
+		String placeholder = element.getDomAttribute("placeholder");
+		
+		try {
+			//Return the description based on available attribute
+			if(isEmptyOrNull(name)){
+				return ("Element name: " + truncate(name,50));
+			} else if(isEmptyOrNull(id)){
+				return ("Element id: " + truncate(id,50));
+			} else if(isEmptyOrNull(text)){
+				return ("Element text: " + truncate(text,50));
+			} else if(isEmptyOrNull(className)){
+				return ("Element className: " + truncate(className,50));
+			} else if(isEmptyOrNull(placeholder)){
+				return ("Element placeholder: " + truncate(placeholder,50));
+			}
+		} catch (Exception e) {
+			logger.error("Description not available for " + locator.toString());
+			logger.error("Error message: " + e.getMessage());
+		}
+		
+		return ("Description not available for " + locator.toString());
+	}
+	
+	//utility method to check if string is empty or null
+	private boolean isEmptyOrNull(String text) {
+		return (text != null && text.isEmpty());
+	}
+	
+	//utility method to truncate long String
+	private String truncate(String text, int maxLength) {
+		if(text == null || text.length() <= maxLength) {
+			return text;
+		} else {
+			return (text.substring(0, maxLength) + "...");
 		}
 	}
 }
